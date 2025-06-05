d55a7f69cf0e009dc89705fa68d5d74c25a42608
anjaliingle111 <ingle.a@northeastern.edu>
2025-06-05 10:05:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from src.eda import plot_gender_distribution, plot_correlation
+import pandas as pd
+
+def main():
+    df = pd.read_csv("data/diabetic_data.csv")
+    plot_gender_distribution(df)
+    plot_correlation(df)
+
+if __name__ == "__main__":
+    main()
 
